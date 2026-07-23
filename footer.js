77fd23fe79845fb48eb438f51464d9cdf35ccafc
Marcus Lu <lq776138310@gmail.com>
2026-07-23 14:12:41 +0800

    111
    1

--- footer.js
+++ footer.js
@@ -2,16 +2,16 @@
   const footerConfig = {
     brandAlt: '中国签证申请服务中心',
     description: '签证服务的官方提供商，致力于为所有前往中国的申请人提供高质量、专业的协助。',
-    copyright: 'COPYRIGHT ©2023CVASC, ALL RIGHTS RESERVED',
+    copyright: 'COPYRIGHT ©2023CVASC, ALL RIGHTS RESERVED&nbsp;京ICP备19020028号-2',
     columns: [
-      { title: '服务项目', links: ['申请签证', 'VIP服务', '进度查询', '旅游和保险'] },
-      { title: '相关信息', links: ['关于我们', '公告', '隐私政策', '法律声明'] },
-      { title: '服务项目', links: ['申请签证', 'VIP服务', '进度查询', '旅游和保险'] }
+      { title: '关于我们', links: ['关于签证中心', '联系我们', '法律条款'] },
+      { title: '友情链接', links: [] },
+      { title: '服务反馈', links: [] }
     ]
   };
 
   const footerColumn = ({ title, links }) => `<section class="content-footer-column"><h2>${title}</h2><ul>${links.map((label) => `<li><a href="#">${label}</a></li>`).join('')}</ul></section>`;
-  const footerTemplate = `<footer class="content-footer" data-content-footer="true"><div class="content-footer-inner"><section class="content-footer-brand"><img src="images/logo5.png" alt="${footerConfig.brandAlt}"><p>${footerConfig.description}</p></section><div class="content-footer-links">${footerConfig.columns.map(footerColumn).join('')}</div></div><div class="content-footer-copyright">${footerConfig.copyright}</div></footer>`;
+  const footerTemplate = `<footer class="content-footer" data-content-footer="true"><div class="content-footer-inner"><section class="content-footer-brand"><img src="images/logo5.png" alt="${footerConfig.brandAlt}"><p>${footerConfig.description}</p></section><div class="content-footer-links">${footerConfig.columns.map(footerColumn).join('')}</div><div class="content-footer-copyright">${footerConfig.copyright}</div></div></footer>`;
 
   const contentMain = [...document.querySelectorAll('.main-height > main')].find((main) => main.getAttribute('aria-hidden') !== 'true');
   if (!contentMain || contentMain.querySelector('[data-content-footer="true"]')) return;
